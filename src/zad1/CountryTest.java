b0754403900cf2104ca1608da946e96c5e7d8252
Piotr Komosa <komosa.piotr@gmail.com>
2019-11-17 15:51:23 +0100

    commit1

---
+++ src/zad1/CountryTest.java
@@ -0,0 +1,43 @@
+package zad1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountryTest {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader(new File("countries.csv")))) {
+            Map<String, Country> customerMap = getCountries(br);
+            printCustomer(customerMap);
+        } catch (IOException | InputMismatchException ex) {
+            System.err.println(ex);
+        }
+    }
+    private static void printCustomer(Map<String, Country> customerMap) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje:");
+        String indeks = scanner.nextLine();
+        if(customerMap.containsKey(indeks)){
+            System.out.println(customerMap.get(indeks));
+        } else {
+            System.out.println("Kraj o takim kodzie nie istnieje");
+        }
+    }
+        private static Map<String, Country> getCountries(BufferedReader br) throws IOException{
+            Map<String, Country> countiresMap = new HashMap<>();
+            String line;
+            while ((line = br.readLine()) !=null){
+                String[] country = line.split(";");
+                String code = country[0];
+
+                countiresMap.put(code, new Country(code,country[1],country[2]));
+            }
+            return countiresMap;
+        }
+    }
