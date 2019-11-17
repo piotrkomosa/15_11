09b76a6c76f69e95635134f0e7e41b8748681e27
Piotr Komosa <komosa.piotr@gmail.com>
2019-11-17 16:12:32 +0100

    commit3

--- src/zad1/Country.java
+++ src/zad1/Country.java
@@ -1,41 +1,32 @@
 package zad1;
 
 public class Country {
-
     private String code;
     private String name;
     private String population;
-
     public Country(String code, String name, String population) {
         this.code = code;
         this.name = name;
         this.population = population;
     }
-
     public String getCode() {
         return code;
     }
-
     public void setCode(String id) {
         this.code = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getPopulation() {
         return population;
     }
-
     public void setPopulation(String population) {
         this.population = population;
     }
-
     @Override
     public String toString() {
         return name+"("+code+")"+ " ma "+ population + " ludności";
